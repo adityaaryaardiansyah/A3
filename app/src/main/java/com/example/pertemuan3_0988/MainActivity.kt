34add60ea9b6ec5c1d2dedce6cb90a3bf4ee3919
adityaaryaardiansyah <xeegaming4@gmail.com>
2024-03-07 16:29:12 +0700

    Inisial commit

---
+++ app/src/main/java/com/example/pertemuan3_0988/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.pertemuan3_0988
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.pertemuan3_0988.ui.theme.PERTEMUAN3_0988Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PERTEMUAN3_0988Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PERTEMUAN3_0988Theme {
+        Greeting("Android")
+    }
+}
